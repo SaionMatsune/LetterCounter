ca849a152ac0077dbe921a2434a92d736c124a9b
SaionMatsune <>
2016-02-21 22:36:42 +0900

    Add Scroll in JTextArea.

--- src/frame/MainFrame.java
+++ src/frame/MainFrame.java
@@ -4,9 +4,11 @@ import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+
 
 import java.awt.Container;
 import java.awt.BorderLayout;
@@ -23,6 +25,7 @@ public class MainFrame extends JFrame {
     private static MainFrame frame = new MainFrame();
 
     public static JTextArea area;
+    private static JScrollPane sp;
     private static JButton b1;
     private static JButton b2;
     public static JLabel label;
@@ -37,39 +40,42 @@ public class MainFrame extends JFrame {
 		}
 
                 
-                
                 JPanel p = new JPanel();
 
                 getButton();
-                getJTextArea();
-                getLabel();
+                getTextArea();
+                getLabel();            
                 
-                p.add(area);
+                p.add(sp);
                 p.add(b1);
-                p.add(b2);
-                                
+                p.add(b2);                             
+
+                
+                Container contentPane = getContentPane();
+                contentPane.add(p, BorderLayout.CENTER);
+                contentPane.add(label, BorderLayout.SOUTH);
+
+
                 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setResizable(false);
                 setTitle("文字数カウント");
                 setBounds(150, 100, 800, 500);
-                setVisible(true);
-                
-                Container contentPane = getContentPane();
-                contentPane.add(p, BorderLayout.CENTER);
-                contentPane.add(label, BorderLayout.SOUTH);
-                                
+                setVisible(true);                
         }
 
         public static MainFrame getInstance(){
                 return frame;
 	}
         
-        private static void getJTextArea() {
-                area = new JTextArea("");
-                area.setPreferredSize(new Dimension(780, 300));
+        private static void getTextArea() {
+                area = new JTextArea();
                 area.setLineWrap(true);
                 area.setWrapStyleWord(true);
+                
+                sp = new JScrollPane(area);
+                sp.setPreferredSize(new Dimension(780, 300));
+                sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         }
 
         private void getButton() {
@@ -81,6 +87,10 @@ public class MainFrame extends JFrame {
         
         private void getLabel(){
             label = new JLabel();
+        }
+        
+        private void getMenu(){
+            
         }
         
         // Count
